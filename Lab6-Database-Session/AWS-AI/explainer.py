ad154adc2acd1f6ba6c4eebe17bb0747681a452d
Ken Graf <ken.graf@cs.unh.edu>
2026-03-04 11:02:53 -0500

    Update explainer.py

--- Lab6-Database-Session/AWS-AI/explainer.py
+++ Lab6-Database-Session/AWS-AI/explainer.py
@@ -1,75 +1,18 @@
 import json
 import boto3
+import re
 
-
-allModels = {
-	"A1":"us.amazon.nova-2-lite-v1:0",
-	"A2":"us.amazon.nova-lite-v1:0",
-	"A3":"us.amazon.nova-micro-v1:0",
-	"A4":"us.amazon.nova-premier-v1:0",
-	"A5":"us.amazon.nova-pro-v1:0",
-	"C1":"us.anthropic.claude-3-5-haiku-20241022-v1:0",
-	"C2":"us.anthropic.claude-3-5-sonnet-20240620-v1:0",
-	"C3":"us.anthropic.claude-3-5-sonnet-20241022-v2:0",
-	"C4":"us.anthropic.claude-3-7-sonnet-20250219-v1:0",
-	"C5":"us.anthropic.claude-3-haiku-20240307-v1:0",
-	"C6":"us.anthropic.claude-haiku-4-5-20251001-v1:0",
-	"C7":"us.anthropic.claude-opus-4-1-20250805-v1:0",
-	"C8":"us.anthropic.claude-opus-4-20250514-v1:0",
-	"C9":"us.anthropic.claude-opus-4-5-20251101-v1:0",
-	"CA":"us.anthropic.claude-opus-4-6-v1",
-	"CB":"us.anthropic.claude-sonnet-4-20250514-v1:0",
-	"CC":"us.anthropic.claude-sonnet-4-5-20250929-v1:0",
-	"CD":"us.anthropic.claude-sonnet-4-6",
-	"D1":"us.deepseek.r1-v1:0",
-	"M1":"us.meta.llama3-1-405b-instruct-v1:0",
-	"M2":"us.meta.llama3-1-70b-instruct-v1:0",
-	"M3":"us.meta.llama3-1-8b-instruct-v1:0",
-	"M4":"us.meta.llama3-2-11b-instruct-v1:0",
-	"M5":"us.meta.llama3-2-1b-instruct-v1:0",
-	"M6":"us.meta.llama3-2-3b-instruct-v1:0",
-	"M7":"us.meta.llama3-2-90b-instruct-v1:0",
-	"M8":"us.meta.llama3-3-70b-instruct-v1:0",
-	"M9":"us.meta.llama4-maverick-17b-instruct-v1:0",
-	"MA":"us.meta.llama4-scout-17b-instruct-v1:0",
-	"P1":"us.mistral.pixtral-large-2502-v1:0",
-	"S1":"us.stability.stable-conservative-upscale-v1:0",
-	"S2":"us.stability.stable-creative-upscale-v1:0",
-	"S3":"us.stability.stable-fast-upscale-v1:0",
-	"S4":"us.stability.stable-image-control-sketch-v1:0",
-	"S5":"us.stability.stable-image-control-structure-v1:0",
-	"S6":"us.stability.stable-image-erase-object-v1:0",
-	"S7":"us.stability.stable-image-inpaint-v1:0",
-	"S8":"us.stability.stable-image-remove-background-v1:0",
-	"S9":"us.stability.stable-image-search-recolor-v1:0",
-	"SA":"us.stability.stable-image-search-replace-v1:0",
-	"SB":"us.stability.stable-image-style-guide-v1:0",
-	"SC":"us.stability.stable-outpaint-v1:0",
-	"SD":"us.stability.stable-style-transfer-v1:0",
-	"T1":"us.twelvelabs.pegasus-1-2-v1:0",
-	"W1":"us.writer.palmyra-x4-v1:0",
-	"W2":"us.writer.palmyra-x5-v1:0",
-	"X1":"google.gemma-3-27b-it",
-	"X2":"minimax.minimax-m2.1",
-	"X3":"moonshotai.kimi-k2.5",
-	"X4":"nvidia.nemotron-nano-3-30b",
-	"X5":"openai.gpt-oss-120b-1:0",
-	"X6":"qwen.qwen3-235b-a22b-2507-v1:0",
-	"X7":"zai.glm-4.7"
-}
-
-recommendedModels = {
-"A3":"us.amazon.nova-micro-v1:0",
-"C1":"us.anthropic.claude-3-5-haiku-20241022-v1:0",
-"CC":"us.anthropic.claude-sonnet-4-5-20250929-v1:0",
-"C9":"us.anthropic.claude-opus-4-5-20251101-v1:0",
-"X1":"google.gemma-3-27b-it",
-"X5":"openai.gpt-oss-120b-1:0",
-}
+def valid_simple_pgn(text):
+	pattern = re.compile(r'^(\d+\.\s\S{1,5}\s\S{1,5}\s){1,10}$')
+    return bool(pattern.fullmatch(text))
 
 def buildPrompt(event):
+	# Chekcks to prevent attacher fromusing this features for their own prompts
 	move = event.get("move")
+	if( len(movr) > 7) return null
 	pgn = event.get("pgn")
+	if( not valid_simple_pgn(pgn) ) return null
+		
 	prompt = f"Analyze the chess move {move} move after: {pgn}. Be as detailed and specific as possible."
 	prompt += """
 Please provide:
